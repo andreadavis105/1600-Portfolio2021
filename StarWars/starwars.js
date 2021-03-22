65b2fc86126b7dd02bf61be10242a3d1c7a30e83
Andrea Davis <andrearuthdavis105@gmail.com>
2021-03-21 21:34:31 -0600

    added movie posters and some styling

--- StarWars/starwars.js
+++ StarWars/starwars.js
@@ -8,11 +8,24 @@ movieTwo.textContent = films[1].title
 
 console.log(films[0].title) */
 
-let movietitle = document.querySelector(".movieTitle")
+let movieList = document.querySelector(".movieList")
 
 for (let i = 0; i < films.length; i++) {
-    let title = films[i].title
-    let newTitle = document.createElement("li")
-    newTitle.textContent = title
-    movietitle.appendChild(newTitle)
+    const filmLookup = films.find(film => getMovieNumber(film.url) === (i + 1))
+    let figure = document.createElement("figure")
+    let newPoster = document.createElement("img")
+    let figCaption= document.createElement("figcaption")
+    newPoster.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+    figCaption.textContent = filmLookup.title
+    
+    figure.appendChild(newPoster)
+    figure.appendChild(figCaption)
+    movieList.appendChild(figure)
+
+    
+
+}
+function getMovieNumber(url) {
+    let movNum = url.charAt(url.length - 2)
+    return parseInt(movNum, 10)
 }
