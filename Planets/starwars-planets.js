5542ee7e9e11d4f729e7d1bace559021b22e4598
Andrea Davis <andrearuthdavis105@gmail.com>
2021-03-26 00:05:32 -0400

    got planets working

--- Planets/starwars-planets.js
+++ Planets/starwars-planets.js
@@ -1,2 +1,37 @@
 import { planets} from '../data/planets.js'
+import { getLastNum, removeChildren} from '../UtilityFunctions/index.js'
 
+const main = document.querySelector("main")
+const planetsList = document.querySelector(".planetNames")
+const planetPhotos = document.querySelector(".photos")
+
+
+function navigation() {
+    planets.forEach(planet => {
+        let anchor = document.createElement("a")
+        anchor.href = "#"
+        anchor.addEventListener("click", () => {
+            displayPhoto(planet)
+        })
+        let item = document.createElement("li")
+        item.textContent = planet.name
+
+        anchor.appendChild(item)
+        planetsList.appendChild(anchor)
+    })
+}
+
+function displayPhoto(property) {
+    removeChildren(planetPhotos)
+    let image = document.createElement("img")
+    let imageNum = getLastNum(property.url)
+    image.src = `https://starwars-visualguide.com/assets/img/planets/${imageNum}.jpg`
+    //fix the broken images icon from displaying
+    image.addEventListener("error", () => {
+        image.hidden = true
+    })
+    planetPhotos.appendChild(image)
+}
+
+navigation()
+//displayPhoto()  -already called in the navigation function/forEach loop
