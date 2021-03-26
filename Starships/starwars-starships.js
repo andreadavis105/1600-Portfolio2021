3f3c99ab4ffd18ad095f3e2fb29afc3e761a3951
Andrea Davis <andrearuthdavis105@gmail.com>
2021-03-25 21:38:05 -0400

    added star field to starships

--- Starships/starwars-starships.js
+++ Starships/starwars-starships.js
@@ -1,4 +1,5 @@
 import { starships } from '../data/starships.js'
+import { getLastNum, removeChildren } from '../UtilityFunctions/index.js'
 
 const main = document.querySelector("main")
 const navList = document.querySelector(".shipsList")
@@ -21,10 +22,42 @@ function popNav() {
 }
 
 function popShips(shipData) {
+    removeChildren(shipView)
     let shipImage = document.createElement("img")
-    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${starShip}.jpg`
+    let shipNum = getLastNum(shipData.url)
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    //fix a broken image
+    shipImage.addEventListener("error", () => {
+    shipImage.hidden = true
+    })
     shipView.appendChild(shipImage)
 }
 
+
 popNav()
-popShips()
+//popShips()
+
+function starField(element, numStars) {
+    element.style.setProperty("background-color", "black")
+    for (let i = 0; i < numStars; i++) {
+        let star = document.createElement("div")
+        star.style.setProperty("width", "2px")
+        star.style.setProperty("height", "2px")
+        star.style.setProperty("background-color", "white")
+        let xy = randomLocation()
+        star.style.left = `${xy[0]}px`
+        star.style.top = `${xy[1]}px`
+        star.style.setProperty("position", "absolute")
+        element.appendChild(star)
+    }
+}
+
+function randomLocation() {
+    let y = document.body.scrollHeight
+    let x = document.body.scrollWidth
+    let randomY = Math.floor(Math.random() * y)
+    let randomX = Math.floor(Math.random() * x)
+    return [randomX, randomY]
+}
+
+starField(document.querySelector("body"), 400)
