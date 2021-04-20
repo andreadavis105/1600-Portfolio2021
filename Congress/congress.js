a3466fc66a0761bddef16aee4b5342b81b2cfa14
Andrea Davis <andrearuthdavis105@gmail.com>
2021-04-20 16:38:11 -0600

    got all Congress members figures showing up

--- Congress/congress.js
+++ Congress/congress.js
@@ -6,7 +6,36 @@ const congressGrid = document.querySelector('.membersGrid')
 const seniority = document.querySelector('.seniority')
 const birthday = document.querySelector('.birthday')
 
-function display
+function displayMembers(simpleList) {
+    simpleList.forEach(person => {
+        let personDiv = document.createElement('div')
+        let personFigure = document.createElement('figure')
+        let figureImg = document.createElement('img')
+        let figureCap = document.createElement('figcaption')
+
+        figureImg.src = person.imgURL
+        figureCap.textContent = person.name
+
+        personFigure.appendChild(figureImg)
+        personFigure.appendChild(figureCap)
+        personDiv.appendChild(personFigure)
+        congressGrid.appendChild(personDiv)
+    })
+}
+
+
+function getImpInfo(peopleList) {
+    return peopleList.map(person => {
+        let middleName = person.middle_name ? `${person.middle_name}` : ``
+        return {
+            id: person.id,
+            name: `${person.first_name}${middleName} ${person.last_name}`,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-100px.jpeg`
+        }
+    })
+}
+
+displayMembers(getImpInfo(representatives))
 
 
 
