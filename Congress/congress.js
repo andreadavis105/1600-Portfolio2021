772b1543c802243c2930d3e7f92c790db0f5a91e
Andrea Davis <andrearuthdavis105@gmail.com>
2021-04-19 23:27:26 -0600

    filter, map and reduce methods for the reps page

---
+++ Congress/congress.js
@@ -0,0 +1,30 @@
+import { senators } from '../data/senators.js'
+import { representatives } from '../data/representatives.js'
+
+
+const congressGrid = document.querySelector('.membersGrid')
+const seniority = document.querySelector('.seniority')
+const birthday = document.querySelector('.birthday')
+
+function display
+
+
+
+
+
+
+
+
+
+
+/* const republicanButton = document.querySelector('.republicans')
+
+republicanButton.addEventListener('click', () => {
+    displayRepublicans()
+})
+
+function displayRepublicans() {
+    const republicanMembers = representatives.filter((representative) => {
+        return representative.party === 'R'
+    })
+} */
